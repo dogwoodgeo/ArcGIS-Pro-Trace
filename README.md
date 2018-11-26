13f21bee97cd878f59fee47719424fc96d23902f
Bradley Jones <bjones@dogwoodgeo.com>
2018-11-26 14:17:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+---
+
+---
+
+# **ArcGIS-Pro-Trace**
+
+**TODO**
+
+
+
+This tool is also apart of the larger [LRWRA add-in](https://github.com/dogwoodgeo/ArcGIS-Pro-LRWRA) 
+
+**Note**: I am a novice .Net developer, at best.  The add-in works, but feel free to let me know if you see any code that is "wrong" or just plain bad.  
+
+## Getting Started
+
+Clone or download the repository. Open Visual Studio 2015 or 2017, build solution, launch ArcGIS Pro and check the **Add-Ins** tab.
+
+**TODO**
+
+
+
+### Prerequisites
+
+```
+Language:				C#
+Contributor:			Bradley Jones, bjones@dogwoodgeo.com
+Organization:			Little Rock Water Reclamation Authority (LRWRA)
+Org Site: 				https://git.lrwu.com/portal
+Date:					10/31/2018
+ArcGIS Pro:				2.2
+ArcGIS Pro SDK: 		2.2
+Visual Studio: 			2017, 2015
+.NET Target Framework:	4.6.1
+```
+
+### How To Use Add-In
+
+1. Build add-in (see **Getting Started**)
+
+2. Launch ArcGIS Pro.
+
+3. Go to **Add-In** tab.
+
+   **TODO**
+
+
+
---
+++ Trace/TraceUpstream.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Editing.Attributes;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace Trace
+{
+    internal class TraceUpstream : MapTool
+    {
+        public TraceUpstream()
+        {
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Rectangle;
+            SketchOutputMode = SketchOutputMode.Screen;
+        }
+        protected override Task OnToolActivateAsync(bool active)
+        {
+            return QueuedTask.Run(() =>
+            {
+
+                var map = MapView.Active.Map;
+                map.SetSelection(null);
+                var mhExists = map.GetLayersAsFlattenedList().OfType<FeatureLayer>().Any(m => m.Name == "Manholes");
+                var sewerExists = map.GetLayersAsFlattenedList().OfType<FeatureLayer>().Any(s => s.Name == "Sewer Lines");
+                if (mhExists == false && sewerExists == false)
+                {
+                    MessageBox.Show("Manholes & Sewers are missing from map.", "Message");
+                }
+                else if (mhExists == false && sewerExists)
+                {
+                    MessageBox.Show("Sewer Lines layer is present. \n\nManholes layer is missing from map.", "Message");
+                }
+                else if (mhExists && sewerExists == false)
+                {
+                    MessageBox.Show("Manholes layer is present. \n\nSewers layer is missing from map.", "Message");
+                }
+                else
+                {
+                    var layers = map.GetLayersAsFlattenedList().OfType<FeatureLayer>();
+                    foreach (var layer in layers)
+                    {
+
+                        if (layer.Name == "Manholes")
+                        {
+                            layer.SetSelectable(true);
+                        }
+                        else
+                        {
+                            layer.SetSelectable(false);
+                        }
+                    }
+                }
+            });
+        }
+
+        protected override Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+            return QueuedTask.Run(() =>
+            {
+                try
+                {
+                    ActiveMapView.SelectFeatures(geometry, SelectionCombinationMethod.New);
+
+
+                    var map = MapView.Active.Map;
+ 
+                    // get the currently selected features in the map
+                    var selectedFeatures = map.GetSelection();
+
+                    if (selectedFeatures.Count == 0)
+                    {
+                        MessageBox.Show("No manhole was selected.\n\nTry selecting a manhole again.", "Warning");
+                    }
+                    else if (selectedFeatures.Count > 1)
+                    {
+                        MessageBox.Show("More than one manhole selected.\n\nTry selecting manholes again.", "Warning");
+                    }
+                    else
+                    {
+                        // get the first layer and its corresponding selected feature OIDs
+                        var firstSelectionSet = selectedFeatures.First();
+
+                        // create an instance of the inspector class
+                        var inspector = new Inspector();
+
+                        // load the selected features into the inspector using a list of object IDs
+                        inspector.Load(firstSelectionSet.Key, firstSelectionSet.Value);
+
+                        //get the value of
+                        string mhNum = inspector["MH_NO"].ToString();
+                        MessageBox.Show(mhNum, "Selected Manhole");
+
+                    }
+
+                }
+
+                catch (Exception)
+                {
+                    string caption = "Failed to select manhole!";
+                    string message = "Process failed. \n\nSave and restart ArcGIS Pro and try process again.\n\n" +
+                        "If problem persist, contact your local GIS nerd.";
+
+                    //Using the ArcGIS Pro SDK MessageBox class
+                    MessageBox.Show(message, caption);
+                }
+                return true;
+            });
+        }
+    }
+}
